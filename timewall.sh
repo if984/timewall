e0e351e87702b7487d00488b541a0a8fb4b4134c
Muzifs <maksimman2907@gmail.com>
2024-12-08 14:52:44 +0300

    Extended verification for GNOME.

--- timewall.sh
+++ timewall.sh
@@ -4,9 +4,11 @@
 ## Mail : anotherankor@gmail.com
 ## Github : @anotherankor
 
+## Maksim A. Kozhevyatov (if984) left his contribution
+
 # set wallpaper in gnome
 
-if [[ $DESKTOP_SESSION == "gnome" ]] ; then
+if [[ "$DESKTOP_SESSION" =~ ^(GNOME|Gnome|gnome|Gnome-xorg|gnome-xorg)$ ]] ; then
 	gsettings set org.gnome.desktop.background picture-uri file:///"$(pwd)"/images/"$(date +%H)".png
 fi
 
